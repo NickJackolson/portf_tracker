286cedc72d80886f7d6394d6daa25fee8fb2230a
Mete Durlu <meted@linux.ibm.com>
2023-12-24 20:12:35 +0100

    portfolio_tracker: add current value tracking
    Signed-off-by: Mete Durlu <meted@linux.ibm.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::prelude::*;
 use std::io::{Result, BufReader, Lines};
 use std::path::Path;
 use std::path::PathBuf;
+use yahoo_finance_api as yahoo;
+use tokio_test;
 
 #[derive(Debug, Clone, Copy)]
 struct Transaction {
@@ -14,7 +16,8 @@ struct Stock {
 	symbol: String,
 	total_amount: f32,
 	total_investment: f32,
-	transactions: Vec<Transaction>
+	transactions: Vec<Transaction>,
+	current_price: f32
 }
 
 impl Stock {
@@ -24,6 +27,15 @@ impl Stock {
 			total_amount: 0.0,
 			total_investment: 0.0,
 			transactions: Vec::new(),
+			current_price: 0.0,
+		}
+	}
+	
+	fn update_current_price(&mut self, current_price: f32)
+	{
+		self.current_price = current_price;
+		if self.symbol.contains("DE") {
+			self.current_price *= 1.1;
 		}
 	}
 
@@ -36,7 +48,7 @@ impl Stock {
 			self.total_investment *= -1.0;
 			return;
 		}
-		if t.amount < 0.0 && self.total_amount > 0.0 {
+		if (t.amount < 0.0) && (self.total_amount > 0.0) {
 			let avg_price: f32 = self.transactions
 			.iter()
 			.fold(0.0, |acc: f32, x| acc + x.price) / self.transactions.len() as f32;
@@ -44,8 +56,9 @@ impl Stock {
 				amount : self.total_amount,
 				price : avg_price,
 			};
-			self.transactions = vec![after_sale];
-		}  else {
+			self.transactions.clear();
+			self.transactions.push(after_sale);
+		} else {
 			self.transactions.push(t);
 		}
 		self.total_investment += t.price * t.amount;
@@ -60,6 +73,15 @@ impl Stock {
 		} else {
 			println!("Gained Money   : {}", self.total_investment);
 		}
+		let current_value = self.total_amount * self.current_price;
+		let potential = self.get_potential();
+		println!("Current Value  : {}", current_value);
+		println!("Potential Gain : {}", potential);
+	}
+	
+	fn get_potential(&self) -> f32
+	{
+		return self.total_amount * self.current_price - self.total_investment;
 	}
 
 }
@@ -70,7 +92,7 @@ fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>> where P: AsRef<P
 	Ok(BufReader::new(file).lines())
 }
 
-fn stock_vec_add_transaction(mut stock_vec: Vec<Stock>, symbol: String, t: Transaction) -> Vec<Stock>
+fn stock_vec_add_transaction(mut stock_vec: Vec<Stock>, symbol: String, t: Transaction, provider: &yahoo::YahooConnector) -> Vec<Stock>
 {
 	for stock in stock_vec.iter_mut() {
 		if stock.symbol == symbol {
@@ -78,7 +100,10 @@ fn stock_vec_add_transaction(mut stock_vec: Vec<Stock>, symbol: String, t: Trans
 			return stock_vec;
 		}
 	}
+	let response = tokio_test::block_on(provider.get_latest_quotes(&symbol, "1d")).unwrap();
+	let quote = response.last_quote().unwrap();
 	let mut new_stock = Stock::new(symbol);
+	new_stock.update_current_price(quote.close as f32);
 	new_stock.add_transaction(t);
 	stock_vec.push(new_stock);
 	return stock_vec;
@@ -88,14 +113,20 @@ fn create_stock_list(path: PathBuf) -> Vec<Stock>
 {
 	let mut stock_vec: Vec<Stock> = Vec::new();
 
+	let provider = yahoo::YahooConnector::new();
 	if let Ok(lines) = read_lines(path) {
 		for line in lines {
 			if let Ok(transaction_line) = line {
 				let v : Vec<&str> = transaction_line.split(' ').collect();
 				let symbol = String::from(v[0]);
-				let t = Transaction {amount: v[1].parse::<f32>().unwrap(),
-												  price: v[4].parse::<f32>().unwrap()};
-				stock_vec = stock_vec_add_transaction(stock_vec, symbol, t);
+				let amount = v[1].parse::<f32>().unwrap();
+				let mut price = v[4].parse::<f32>().unwrap();
+				if symbol.contains("DE") {
+					price *= 1.1;
+				}
+				let t = Transaction {amount,
+												  price};
+				stock_vec = stock_vec_add_transaction(stock_vec, symbol, t, &provider);
 			}
 		}
 	}
@@ -104,11 +135,14 @@ fn create_stock_list(path: PathBuf) -> Vec<Stock>
 
 fn main()
 {
+	let mut total_potential = 0.0;
 	let cwd_absolute = Path::new(".").canonicalize().unwrap();
 	let data_path = cwd_absolute.join("data").join("transactions.txt");
 	let stock_vec = create_stock_list(data_path);
 	for stock in stock_vec.iter() {
 		println!("====================");
 		stock.print_stock();
+		total_potential += stock.get_potential();
 	}
+	println!("current  potential = {}", total_potential);
 }
